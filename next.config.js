4175ab6e570d8af67b4e4479d8810024c4c124f7
Justin Arista Kasin <justin+github@aristakas.in>
2021-09-22 07:35:24 +0100

    try a fix suggested in github: https://github.com/prisma/prisma/issues/6899#issuecomment-849126557

--- next.config.js
+++ next.config.js
@@ -5,4 +5,11 @@ module.exports = {
   reactStrictMode: true,
   generateEtags: false,
   poweredByHeader: false,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push("_http_common");
+    }
+    return config;
+  },
+  target: serverless,
 };
